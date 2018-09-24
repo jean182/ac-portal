92f8be31df93e9daa77e7c531b4e6c9eef70cd0f
Jean <jean-marco-10@hotmail.com>
2018-09-24 11:37:50 -0600

    Association works

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,6 @@
 class Tag < ApplicationRecord
   belongs_to :checklist, optional: true
   belongs_to :company, optional: true
+  has_many :has_tags, dependent: :destroy
   enum tag_type: [:industry, :expertise]
 end
--- db/migrate/20180924145905_create_has_tags.rb
+++ db/migrate/20180924145905_create_has_tags.rb
@@ -1,9 +1,9 @@
 class CreateHasTags < ActiveRecord::Migration[5.2]
   def change
     create_table :has_tags do |t|
-      t.references :checklists, foreign_key: true
-      t.references :tags, foreign_key: true
-      t.references :companies, foreign_key: true
+      t.references :checklist, foreign_key: true
+      t.references :tag, foreign_key: true
+      t.references :company, foreign_key: true
 
       t.timestamps
     end
