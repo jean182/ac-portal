fd05a943998034786a8e6827ede34160ee5e51b7
Jean <jean-marco-10@hotmail.com>
2018-10-08 09:58:17 -0600

    Convert phase_numbers to integer, in order to use an enum with that, also created a after_create method to add the 4 phases automatically to that company

--- app/models/phase.rb
+++ app/models/phase.rb
@@ -2,6 +2,7 @@
 class Phase < ApplicationRecord
   has_many :checklists, dependent: :destroy
   has_many :milestones, dependent: :destroy
+  enum phase_number: [:first_phase, :second_phase, :third_phase, :fourth_phase]
   enum status: [:inactive, :active, :completed]
   belongs_to :company, dependent: :destroy
 end
